026a5528abfc504d5396046a63906b79b159450a
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-05 02:27:28 +0600

    bom

---
+++ js/bom.js
@@ -0,0 +1,43 @@
+// document.querySelector("#demo1").innerHTML =
+//   "Screen resolution" + window.innerWidth + " x " + window.innerHeight;
+// document.querySelector("#demo2").innerHTML =
+//   "Screen pixel depth = " + screen.pixelDepth;
+// document.querySelector("#demo3").innerHTML =
+//   "Screen color depth = " + screen.colorDepth;
+
+// document.querySelector("#demo4").innerHTML =
+//   "appName: " + window.navigator.appName;
+// document.querySelector("#demo5").innerHTML =
+//   "appCodeName: " + window.navigator.appCodeName;
+// document.querySelector("#demo6").innerHTML =
+//   "platform: " + window.navigator.platform;
+
+// const alert = document.querySelector("#alert");
+// const confirm = document.querySelector("#confirm");
+
+// const confirmDiv = document.querySelector("#confirmDiv");
+
+// function showAlert() {
+//   window.alert("Press ok");
+// }
+
+// function showConfirm() {
+//   if (window.confirm("Do you confirm?")) {
+//     confirmDiv.innerHTML = "Confirmed";
+//   } else {
+//     confirmDiv.innerHTML = "Not Confirmed";
+//   }
+// }
+
+// function showPrompt(){
+    
+//     let userData = window.prompt("What is your mother's birthplace?", "");
+//     if(userData === null || userData === ""){
+//         promptDiv.innerHTML = "You have not asnwered the question.";
+//     }
+//     else{
+//         promptDiv.innerHTML = "your mother's birthplace is " + userData;
+//     }
+// }
+
+
