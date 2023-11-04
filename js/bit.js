d1422751a38f46c07cfe67a0e316e94a6c2a6825
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-05 02:27:07 +0600

    bit

---
+++ js/bit.js
@@ -0,0 +1,6 @@
+// console.log((10).toString(2));
+// console.log((11).toString(2));
+// console.log(`10 AND 11: ${10 & 11}`);
+// console.log(`10 OR 11: ${10 | 11}`);
+// console.log(`10 XOR 11: ${10 ^ 11}`);
+// console.log(`NOT 10: ${ ~10}`);
